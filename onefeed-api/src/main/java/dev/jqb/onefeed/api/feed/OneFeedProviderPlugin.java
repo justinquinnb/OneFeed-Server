f23b119444e4f31b17781eb10f95163f44aba55d
Justin Quinn <justinquinnb@gmail.com>
2026-05-15 02:40:47 -0400

    Create plugin test kit and project BOM

--- onefeed-api/src/main/java/dev/jqb/onefeed/api/feed/OneFeedProviderPlugin.java
+++ onefeed-api/src/main/java/dev/jqb/onefeed/api/feed/OneFeedProviderPlugin.java
@@ -2,26 +2,18 @@ package dev.jqb.onefeed.api.feed;
 
 import dev.jqb.onefeed.api.content.RawContent;
 import dev.jqb.onefeed.api.plugin.OneFeedPlugin;
+import java.util.List;
 
 /**
  * A OneFeed {@link Provider} plugin
  */
 public abstract class OneFeedProviderPlugin extends OneFeedPlugin {
     /**
-     * The environment variables specific to this plugin, as specified in OneFeed's
-     * {@code plugin-env.yaml} and {@code .env} files.
-     */
-    protected ProviderConfig providerConfig;
-
-    /**
      * Constructs a new {@link OneFeedPlugin} with the given {@code providerEnv}.
      * @param providerConfig the environment variables specific to this plugin
-     *
-     * @see #providerConfig
      */
     protected OneFeedProviderPlugin(ProviderConfig providerConfig) {
         super();
-        this.providerConfig = providerConfig;
     }
 
     /**
@@ -29,4 +21,10 @@ public abstract class OneFeedProviderPlugin extends OneFeedPlugin {
      * @return the content {@link Provider} that this plugin provides
      */
     public abstract Provider<? extends RawContent> getProvider();
+
+    /**
+     * Gets the names of the feeds that this plugin provides.
+     * @return a list of the names of the feeds that this plugin is configured to provide
+     */
+    public abstract List<String> getFeedNames();
 }
